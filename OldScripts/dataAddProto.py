7e4f196f56c1b345f02f20f871cd1f92a8928f5b
trevor-moss <144730284+trevor-moss@users.noreply.github.com>
2024-06-03 12:26:49 -0400

    Cleaning up

--- OldScripts/dataAddProto.py
+++
@@ -1,8 +0,0 @@
-import h5py
-import numpy as np
-
-# data2 = np.genfromtxt("csvData/fDRB230328_B1.csv", delimiter=",")
-data = np.genfromtxt("csvData/fDRB230328_B1.csv", dtype=None, skip_header=1, delimiter=",")
-
-
-print(data[:5])
